543c4fef0eda6f523c5b15995ce51af7e297ced4
Holly Wheeler <hollyawheeler.96@gmail.com>
2024-03-01 14:38:49 -0800

    adds darkmode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
       kalina: ["Kalina", "serif"],
-      quicksand: ["Quicksand", "sans-serif"]
+      quicksand: ["Quicksand", "sans-serif"],
     },
     extend: {},
   },
